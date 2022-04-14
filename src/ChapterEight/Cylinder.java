d2e0bfdad6133d0dd64ec65d70f72ea0174d0712
increase <increasebruce@gmail.com>
2022-04-14 15:15:13 +0100

    ChapterEight

---
+++ src/ChapterEight/Cylinder.java
@@ -0,0 +1,10 @@
+package ChapterEight;
+/*
+(Cylinder Class) Create a class Cylinder with attributes radius and height , each of
+which has a default value of 1. Provide a method that calculates the cylinders’ volume, which
+is pi multiplied by the square of the radius multiplied by the height . It has set and get meth-
+ods for both radius and height . The set method should verify that radius and height are pos-
+itive numbers. Write a program to test class Cylinder .
+ */
+public class Cylinder {
+}
