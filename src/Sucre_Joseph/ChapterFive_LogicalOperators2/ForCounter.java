4419eeccb745ded222f1f888944f62387f351b05
increase <increasebruce@gmail.com>
2022-04-14 15:45:08 +0100

    Practices

---
+++ src/Sucre_Joseph/ChapterFive_LogicalOperators2/ForCounter.java
@@ -0,0 +1,12 @@
+package Sucre_Joseph.ChapterFive_LogicalOperators2;
+/*
+Counter-controlled iteration with the for iteration statement.
+ */
+public class ForCounter {
+    public static void main(String[] args) {
+        for (int counter = 1; counter <= 10; counter++){
+            System.out.printf("%d ", counter );
+        }
+        System.out.println();
+    }
+}
---
+++ src/Sucre_Joseph/ChapterSix__Methods/Maximum.java
@@ -0,0 +1,33 @@
+package Sucre_Joseph.ChapterSix__Methods;
+
+import java.util.Scanner;
+
+/*
+Use a method called maximum to determine and return the largest of three double values.
+ */
+public class Maximum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter three floating numbers: ");
+        double number1 = scanner.nextDouble();
+        double number2 = scanner.nextDouble();
+        double number3 = scanner.nextDouble();
+
+       double result = maximumNumber( number1, number2,  number3);
+        System.out.println("Maximum is:  " + result);
+
+    }
+    public static double maximumNumber(double a, double b, double c){
+        double maximumValue= a;
+
+        if(b > maximumValue){
+            maximumValue = b;
+
+        }
+        if (c > maximumValue){
+            maximumValue = c;
+        }
+    return maximumValue;
+}
+}
