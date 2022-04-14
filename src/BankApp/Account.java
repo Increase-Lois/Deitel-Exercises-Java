0f40a080bd7ce77299b265bea7961ee00ead5f19
increase <increasebruce@gmail.com>
2022-04-14 15:31:13 +0100

    BankApp

---
+++ src/BankApp/Account.java
@@ -0,0 +1,65 @@
+//package BankApp;
+////package BankApp;
+//
+//public class Account {
+//    private String name;
+//    private String pin;
+//    private int balance = 50_000; //instance variable
+//
+//
+//    public Account(String accountName, String accountPin) {//constructor//
+//        name = accountName;
+//        pin = accountPin;
+//    }
+//
+//    public String getAccountName() {
+//        return name;
+//    }
+//
+//    public int getBalance(String accountPin) {
+//        if (accountPin.equals(pin)) return balance;
+//        else return 0;
+//    }
+//
+//
+//
+////    public int deposit(int amount) {
+////        if (amount >= 0) {
+////            balance = amount + balance;
+////        }
+////        return balance;
+////    }
+//
+//    public int withdraw(int amount, String accountPin) {
+//
+//
+//
+//}
+//
+//
+//
+//    public int withdraw(int amount, String accountPin) {
+//        if (amount >= 0) {
+//            if (amount <= 0) {
+//                return balance;
+//            }
+//        }
+//
+//
+//    }
+//
+//    public int deposit(int amount, String accountPin) {
+//        if (amount>=0){
+//            //if (amount<0);
+//        }
+//  //  }
+//}
+////}
+//
+//
+//
+//
+//
+//
+//
+//
